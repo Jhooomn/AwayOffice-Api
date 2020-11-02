828213856328789e4ac23ad47ee677d8a3fdb131
abdul0214 <abdul.wahab0214@gmail.com>
2020-11-02 23:00:23 +0200

    adding CORS annotation

--- src/main/java/awayoffice/api/AwayofficeApplication.java
+++ src/main/java/awayoffice/api/AwayofficeApplication.java
@@ -2,7 +2,7 @@ package awayoffice.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//Saved working directory and index state WIP on abduls: cd9206a Trigger Heroku deploy after enabling collectstatic
 @SpringBootApplication
 public class AwayofficeApplication {
 
