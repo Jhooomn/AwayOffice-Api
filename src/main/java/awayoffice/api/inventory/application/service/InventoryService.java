2bfc40a02fbe4be1462d2c627d04de74578eabf1
abdul0214 <abdul.wahab0214@gmail.com>
2020-11-06 22:37:59 +0200

    AW-35 #comment Started:List view complete on /api/inventory/assetmodel #in-progress

--- src/main/java/awayoffice/api/inventory/application/service/InventoryService.java
+++ src/main/java/awayoffice/api/inventory/application/service/InventoryService.java
@@ -5,13 +5,18 @@
  */
 package awayoffice.api.inventory.application.service;
 
+import awayoffice.api.hr.application.dto.EmployeeDTO;
+import awayoffice.api.hr.domain.model.Employee;
 import awayoffice.api.inventory.application.dto.AssetModelDTO;
 import awayoffice.api.inventory.domain.model.AssetModel;
 import awayoffice.api.inventory.domain.repository.AssetModelRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @Service
 public class InventoryService {
@@ -24,10 +29,19 @@ public class InventoryService {
 
     public AssetModelDTO getAssetModelById(Long id){
         AssetModel assetModel = assetModelRepository.findById(id).orElse(null);
-        if(assetModel == null){
-            return null;
+        if(assetModel != null){
+            return assetModelAssembler.toModel(assetModel);
             }
-        return assetModelAssembler.toModel(assetModel);
+        return null;
+    }
+
+    public CollectionModel<AssetModelDTO> getAllAssetModels() throws Exception{
+        List<AssetModel> assetModels = assetModelRepository.findAll();
+
+        if(assetModels.size() <= 0){
+            return null;
+        }
+        return assetModelAssembler.toCollectionModel(assetModels);
     }
 
 
