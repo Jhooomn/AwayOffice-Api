7130323937908b320048eb96c1043dea5426dac2
abdul0214 <abdul.wahab0214@gmail.com>
2020-11-06 18:04:41 +0200

    test commit

--- src/main/java/awayoffice/api/AwayofficeApplication.java
+++ src/main/java/awayoffice/api/AwayofficeApplication.java
@@ -2,11 +2,9 @@ package awayoffice.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.test.context.jdbc.Sql;
 
 //Saved working directory and index state WIP on abduls: cd9206a Trigger Heroku deploy after enabling collectstatic
 @SpringBootApplication
-@Sql({"/data.sql"})
 public class AwayofficeApplication {
 
     public static void main(String[] args) {
