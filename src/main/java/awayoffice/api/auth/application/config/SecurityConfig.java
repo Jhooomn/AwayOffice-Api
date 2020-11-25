907215ad1bcdd639c359597e9b1db3bb05d4408d
Muhammad Zubair <Muhammad Zubair@DESKTOP-A92JIVF>
2020-11-25 20:56:14 +0200

    AW-44 #comment Global CORS added in SecurityConfig #done

--- src/main/java/awayoffice/api/auth/application/config/SecurityConfig.java
+++ src/main/java/awayoffice/api/auth/application/config/SecurityConfig.java
@@ -19,6 +19,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -63,6 +68,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .authenticated();
     http.addFilterBefore(jwtAuthenticationFilter(),
                          UsernamePasswordAuthenticationFilter.class);
+  }
+
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(Arrays.asList(
+            "http://localhost:3000",
+            "https://awayoffice.web.app",
+            "http://awayoffice-c2b26.web.app"));
+    configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","PATCH","DELETE"));
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
   }
 
   @Bean
