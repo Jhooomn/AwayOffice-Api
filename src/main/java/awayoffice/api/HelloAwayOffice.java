f45188f6636069aa13c9a4d143ee4fcd2458e5b2
abdul0214 <abdul.wahab0214@gmail.com>
2020-11-27 09:27:58 +0200

    Added multiple CORS

--- src/main/java/awayoffice/api/HelloAwayOffice.java
+++ src/main/java/awayoffice/api/HelloAwayOffice.java
@@ -1,13 +1,15 @@
 package awayoffice.api;
 
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class HelloAwayOffice {
-    
+
     @GetMapping(path = "/hello")
     public HelloWorldBean getHelloAway(){
         return new HelloWorldBean("Welcome to Away Office");
--- src/main/java/awayoffice/api/hr/rest/HRRestController.java
+++ src/main/java/awayoffice/api/hr/rest/HRRestController.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = {"http://localhost:3000","https://awayoffice.web.app"})
 @RestController
 @RequestMapping("/api/hr")
 public class HRRestController {
