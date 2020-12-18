a5088ab4c5a6faa0f53ac93b1c765b007bb1162b
Jhon Baron <jhon.baron@zabud.com.co>
2020-12-18 17:04:14 -0500

    updated: remove not used imports

--- src/main/java/awayoffice/api/hr/application/dto/EmployeeDTO.java
+++ src/main/java/awayoffice/api/hr/application/dto/EmployeeDTO.java
@@ -4,9 +4,6 @@ import awayoffice.api.hr.domain.model.PersonStatus;
 import lombok.Data;
 import org.springframework.hateoas.RepresentationModel;
 
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-
 @Data
 public class EmployeeDTO extends RepresentationModel<EmployeeDTO> {
     Long id;
