5f00aedc7e473bef1c2bdc6d457533397ba712b0
Jhon Baron <jhon.baron@zabud.com.co>
2020-12-18 16:58:13 -0500

    updated: log was not used, just annotation; removed rest and controller advice to RestControllerAdvice

--- src/main/java/awayoffice/api/auth/application/exception/ExceptionHandler.java
+++ src/main/java/awayoffice/api/auth/application/exception/ExceptionHandler.java
@@ -6,21 +6,23 @@
 package awayoffice.api.auth.application.exception;
 
 import java.nio.file.AccessDeniedException;
+
+import lombok.val;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@ControllerAdvice
-@RestController
+@RestControllerAdvice
 @Slf4j
 public class ExceptionHandler extends ResponseEntityExceptionHandler {
 
   @org.springframework.web.bind.annotation.
   ExceptionHandler({AccessDeniedException.class})
   public final ResponseEntity<String> handleAccessDeniedException() {
-    return new ResponseEntity<String>("Access is Denied", HttpStatus.FORBIDDEN);
+    val errorMessage = "Access is Denied";
+    log.info("[handleAccessDeniedException] ::: {} ::: [handleAccessDeniedException]", errorMessage);
+    return new ResponseEntity<String>(errorMessage, HttpStatus.FORBIDDEN);
   }
 }
