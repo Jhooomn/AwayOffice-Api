c85c374420e3074285580c0e720dbddff47b5319
Jhon Baron <jhon.baron@zabud.com.co>
2020-12-18 17:05:40 -0500

    updated: use @EqualsAndHashCode when @data is implemented

--- src/main/java/awayoffice/api/hr/application/dto/EmployeeDTO.java
+++ src/main/java/awayoffice/api/hr/application/dto/EmployeeDTO.java
@@ -2,9 +2,12 @@ package awayoffice.api.hr.application.dto;
 
 import awayoffice.api.hr.domain.model.PersonStatus;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import org.springframework.hateoas.RepresentationModel;
 
 @Data
+@EqualsAndHashCode
 public class EmployeeDTO extends RepresentationModel<EmployeeDTO> {
     Long id;
     String full_name;
--- src/main/java/awayoffice/api/hr/application/dto/VendorDTO.java
+++ src/main/java/awayoffice/api/hr/application/dto/VendorDTO.java
@@ -1,9 +1,12 @@
 package awayoffice.api.hr.application.dto;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import org.springframework.hateoas.RepresentationModel;
 
 @Data
+@EqualsAndHashCode
 public class VendorDTO extends RepresentationModel<VendorDTO> {
     Long id;
     String name;
