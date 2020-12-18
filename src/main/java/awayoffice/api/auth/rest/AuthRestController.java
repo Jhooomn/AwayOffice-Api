772e3599f3fb7ec1270dfafda0c17979f80c1cb0
Jhon Baron <jhon.baron@zabud.com.co>
2020-12-18 17:03:15 -0500

    updated: be specific to response entity return type

--- src/main/java/awayoffice/api/auth/rest/AuthRestController.java
+++ src/main/java/awayoffice/api/auth/rest/AuthRestController.java
@@ -24,7 +24,7 @@ public class AuthRestController {
   @Autowired private JWTTokenProvider jwtTokenProvider;
 
   @PostMapping()
-  public ResponseEntity
+  public ResponseEntity<JwtResponseDTO>
   authenticateUser(@RequestBody JwtRequestDTO requestDTO) {
     Authentication authentication = authenticationManager.authenticate(
         new UsernamePasswordAuthenticationToken(requestDTO.getUsername(),
